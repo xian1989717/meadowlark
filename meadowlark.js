f125710399d2236ef751b25f0ca1735a7deb60cc
chrisliu <383883040.qq.com>
2018-05-13 13:14:09 +0800

    第二次提交

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,17 @@
 let express = require('express');
 let app = express();
-let handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+let handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) {
+                this._sections = {};
+            }
+            this._scctions[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -11,6 +22,34 @@ let fortunes = [
     'Conquer', 'Rivers', 'Do not', 'You', 'Whenever'
 ];
 
+function getWeatherData() {
+    return {
+        locations: [{
+            name: 'Portland'
+        }, {
+            name: 'Wood'
+        }, {
+            name: 'Jack'
+        }, {
+            name: 'Rose'
+        }, ]
+    }
+}
+
+
+
+let data = [
+    { name: 'peter' }, { name: 'jack' }, { name: 'wood' }, { name: 'rose' }
+]
+
+app.use((req, res, next) => {
+    if (!res.locals.partials) {
+        res.locals.partials = {};
+    }
+    res.locals.partials.weather = getWeatherData();
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -19,6 +58,12 @@ app.get('/about', (req, res) => {
     let randomFortunes = fortunes[Math.floor(Math.random() * fortunes.length)];
     res.render('about', { fortunes: randomFortunes });
 });
+
+app.get('/foo', (req, res) => {
+    res.render('foo', { layout: 'microsite' });
+});
+
+
 app.use((req, res) => {
     res.status(404);
     res.render('404');
